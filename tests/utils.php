b3d69f4795d20d9503d7fe8ecf17c06a41b61b48
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-03-10 12:48:31 +0100

    initial commit

---
+++ tests/utils.php
@@ -0,0 +1,8 @@
+<?php
+namespace tad\WP\Snapshots;
+
+function dataDir($path = null){
+	$dataRoot = __DIR__ . '/data';
+
+	return $path === null ? $dataRoot : $dataRoot . '/' . ltrim($path, '/');
+}
